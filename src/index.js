044efed10e5252c4fdfbbc22b9b59ee57f8ae8c4
Neeraj gaur <124441861+Neeraj110@users.noreply.github.com>
2024-06-22 17:28:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,21 +21,4 @@ connectDB()
     console.log("MONGO db connection failed !!!", err);
   });
 
-import dotenv from "dotenv";
-import connectDB from "./db/indexDB.js";
-import app from "./app.js";
 
-dotenv.config({
-  path: "./.env",
-});
-
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server running on port ${process.env.PORT || 8000}`); // Clarified message
-    });
-  })
-  .catch((error) => {
-    console.error("MongoDB connection failed:", error); // Stronger error message
-    process.exit(1); // Exit on connection failure
-  });
